d220c02c22e0361cbcae7c5c12c7f64ee3e2495e
Eric Doering <ericbenjamindoering@gmail.com>
2022-11-17 22:49:47 -0500

    exercise submission

---
+++ routes/items.test.js
@@ -0,0 +1,78 @@
+// iteprocess.env.NODE_ENV = "test";
+
+const { beforeEach, afterEach, describe, default: test } = require("node:test");
+const request = require("supertest");
+const app = require("../app")
+let items = require("../fakeDb")
+
+let item = { name: "bicycle", price: 300 }
+
+beforeEach(async () => {
+    items.push(item)
+});
+
+afterEach(async () => {
+    items = [];
+});
+
+
+describe("GET /items", async function(){
+    test("Gets a list of all items", async function (){
+        const response = await request(app).get("/items");
+        const { items } = response.body;
+        expect(response.statusCode).toBe(200);
+        expect(items).toHaveLength(1)
+    });
+});
+
+
+describe("GET /items/:name", async function (){
+    test("Gets a single item", async function (){
+      const response = await request(app).get(`/items/${ item.name }`);
+      expect(response.statusCode).toBe(200);
+      expect(response.body.item).toEqual(item);
+    });
+  
+    test("Responds with 404 if can't find item", async function () {
+      const response = await request(app).get("/items/0");
+      expect(response.statusCode).toBe(404);
+    });
+  });
+
+
+describe("POST /items", async function(){
+    test("Creates a new shopping list item", async function(){
+        const response = await request(app).post("/items")
+        .send({ name: "umbrella", price: 50 });
+        expect(response.statusCode).toBe(200);
+        expect(response.body.item).toHaveProperty("name");
+        expect(response.body.item).toHaveProperty("price");
+        expect(response.body.item.name).toEqual("umbrella");
+        expect(response.body.item.price).toEqual(50); 
+    });
+});
+
+
+describe("PATCH /items/:name", async function(){
+    test("Updates a single item", async function(){
+      const response = await request(app)
+        .patch(`/items/${item.name}`)
+        .send({ name: "PS5" });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.item).toEqual({ name: "PS5" });
+    });
+  
+    test("Responds with 404 if can't find item", async function(){
+      const response = await request(app).patch("/items/0");
+      expect(response.statusCode).toBe(404);
+    });
+  });
+
+
+  describe("DELETE /items:name", async function(){
+    test("Deletes a single item", async function(){
+        const response = await request(app).delete("/items/${ item.name }");
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({ message: "Deleted" });
+    });
+  });
